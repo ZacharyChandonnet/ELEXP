89bc7a9708550bda6db4d8493ee2c12609f1c515
ZacharyChandonnet <zach.2001@live.ca>
2024-04-24 17:35:34 -0400

    hover effect

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,14 +4,21 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useAuth } from "../Context/AuthContext";
 import { motion } from "framer-motion";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
 
 const Header = () => {
   const { user } = useUser();
   const { logout } = useAuth();
   const [isClicked, setIsClicked] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleClick = () => {
     setIsClicked(!isClicked);
+  };
+
+  const handleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   const headerVariants = {
@@ -26,52 +33,55 @@ const Header = () => {
       initial="hidden"
       animate="visible"
     >
-      
-        <Link to="/">
-          <img src="LogoD.svg" alt="logo" 
-          style={{width: "150px"}}/>
+      <Link to="/">
+        <img src="LogoD.svg" alt="logo" style={{ width: "150px" }} />
+      </Link>
 
-        </Link>
-     
-
-      <Navbar
-        links={[
-          {
-            url: "/a-propos",
-            name: "À propos",
-            title: "À propos",
-          },
-          {
-            url: "/entrainements",
-            name: "Entrainements",
-            title: "Entrainements",
-          },
-          {
-            url: "/programmes",
-            name: "Programmes",
-            title: "Programmes",
-          },
-          // {
-          //   url: "/forum",
-          //   name: "Forum",
-          //   title: "Forum",
-          // },
-        ]}
-      />
+      <div className="hidden md:flex">
+        <Navbar
+          links={[
+            {
+              url: "/a-propos",
+              name: "À propos",
+              title: "À propos",
+            },
+            {
+              url: "/entrainements",
+              name: "Entrainements",
+              title: "Entrainements",
+            },
+            {
+              url: "/programmes",
+              name: "Programmes",
+              title: "Programmes",
+            },
+            // {
+            //   url: "/forum",
+            //   name: "Forum",
+            //   title: "Forum",
+            // },
+          ]}
+        />
+      </div>
 
       <div className="cursor-pointer" onClick={handleClick}>
-        <p>{user?.name} exp.{user?.experience}</p>
-
+        <p>
+          {user?.name} exp.{user?.experience}
+        </p>
 
         {isClicked && (
           <motion.div
-            initial={{ opacity: 0, }}
-            animate={{ opacity: 1, }}
-            transition={{ duration: .5 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
           >
             <div className="flex flex-col absolute top-12 right-0 bg-dark text-white  m-12">
-              <Link to="/profil" className="mx-2 my-2">Profil</Link>
-              <button  className="mx-2 my-2" onClick={logout}>Déconnexion</button>
+              <Link to="/profil" className="mx-2 my-2">
+                Profil
+              </Link>
+              <button className="mx-2 my-2" onClick={logout}>
+                Déconnexion
+              </button>
             </div>
           </motion.div>
         )}
