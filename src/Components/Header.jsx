b5e480675aa6fa2fd7a90ef67508d09339812bf1
ZacharyChandonnet <zach.2001@live.ca>
2024-05-02 21:39:30 -0400

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -65,7 +65,7 @@ const Header = () => {
             },
             {
               url: "/faq",
-              name: "Faq",
+              name: "FAQ",
               title: "faq",
             },
           ]}
